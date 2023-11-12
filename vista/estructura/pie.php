50e59af9ef8a0b56ca0695cd05af8a35006c3d05
Diego966-b <diegobennjaminn@gmail.com>
2023-11-12 20:23:16 -0300

    pie

--- vista/estructura/pie.php
+++ vista/estructura/pie.php
@@ -2,9 +2,6 @@
 
 ?>
 <footer class="fixed-bottom row footer bg-dark bg-gradient">
-    <div class="row">
-        <p class="text-light text-center mb-2">Integrantes del grupo</p>
-    </div>
     <div class="d-flex justify-content-between">
         <div class="col text-center">
             <p class="text-light mb-2">Diego Benjamin</p>
