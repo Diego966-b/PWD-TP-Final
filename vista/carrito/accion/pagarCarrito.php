b170f92fae7ce074bac6a415108f20a1c0815b15
Diego966-b <diegobennjaminn@gmail.com>
2023-11-14 23:13:27 -0300

    fecha

--- vista/carrito/accion/pagarCarrito.php
+++ vista/carrito/accion/pagarCarrito.php
@@ -2,6 +2,7 @@
     include_once("../../../config.php");
     include_once($ESTRUCTURA."/header.php");
     include_once($ESTRUCTURA."/cabeceraBD.php");
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
     $colDatos = data_submitted();
     /*
     $carrito = new Carrito();
